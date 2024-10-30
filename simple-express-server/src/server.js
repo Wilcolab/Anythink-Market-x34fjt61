a01b3f28e0fd4db06e0ac4a326a885cfd1743020
Peter Szekeli <peter.szekeli@gmail.com>
2024-10-30 17:46:55 +0000

    feat: implement task management endpoints in Express server

--- simple-express-server/src/server.js
+++ simple-express-server/src/server.js
@@ -1,8 +1,33 @@
 // src/server.js
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
 const PORT = 8001;
+
+app.use(bodyParser.json());
+
+let tasks = [
+    "Write a diary entry from the future",
+    "Create a time machine from a cardboard box",
+    "Plan a trip to the dinosaurs",
+    "Draw a futuristic city",
+    "List items to bring on a time-travel adventure"
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/tasks', (req, res) => {
+    const task = req.body.text;
+    tasks.push(task);
+    res.json({ message: 'Task added successfully' });
+});
+
+app.get('/tasks', (req, res) => {
+    res.json({ tasks: tasks });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
